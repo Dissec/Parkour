ebec3403b07fa145198bd6db0f24da5a9e23a45d
Dissec <104654480+Dissec@users.noreply.github.com>
2022-05-06 17:44:38 -0300

    Update binds.lua

--- binds.lua
+++ binds.lua
@@ -1,16 +1,14 @@
 local space = 0x20
+local shift = 0xA0
+local q = 0x51
 function wallkick()
-    mousemoverel(-1000, 0)
+    keypress(q)
     keypress(space)
-    wait(0.005)
+    wait(0.15)
+    keyrelease(q)
     keyrelease(space)
-    wait(0.005)
-    mousemoverel(1000, 0)
-    wait()
-    keypress(space)
-    wait(0.005)
-    keyrelease(space)
-    wait(0.005)
+    v = workspace.joao706.HumanoidRootPart
+    v.Velocity = v.Velocity * Vector3.new(1.2, 0, 1.2)
 end
 function wallboost()
     keypress(0x20)
@@ -21,22 +19,38 @@ function wallboost()
     keyrelease(0x20)
     mousemoverel(-1000, 0)
 end
+function cancel()
+    keypress(q)
+    wait(0.12)
+    keyrelease(q)
+    keypress(0x54)
+    wait(0.08)
+    keyrelease(0x54)
+    wait()
+    keypress(0x54)
+    wait()
+    keyrelease(0x54)
+end
 local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
 
 Mouse.KeyDown:connect(function(func)
 
 local key = func:lower()    
 
-if key == "2" then
+if key == "e" then
     repeat
     task.wait()
     until isrbxactive()
     wallboost()
 end
-if key == "3" then
+if key == "f" then
     repeat
     task.wait()
     until isrbxactive()
     wallkick()
+end
+if key == "r" then
+    repeat task.wait() until isrbxactive()
+    cancel()
 end
 end)
