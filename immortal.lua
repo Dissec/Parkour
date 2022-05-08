130efaf3b7abd5224b9d3a6bfbf307b9a4f29ade
Dissec <104654480+Dissec@users.noreply.github.com>
2022-05-08 01:11:57 -0300

    Update immortal.lua

--- immortal.lua
+++ immortal.lua
@@ -1,6 +1,7 @@
+
 game:GetService("RunService").Heartbeat:Connect(function()
     local pool = workspace.Pad.Pillow
-    pool.Position = workspace.joao70006.HumanoidRootPart.Position - Vector3.new(0, 17, 0)
+    pool.Position = workspace.holy1ne.HumanoidRootPart.Position - Vector3.new(0, 17, 0)
     pool.Size = Vector3.new(15, 15, 15)
     pool.Transparency = 1
 end)
