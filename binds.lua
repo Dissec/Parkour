5506a03c9d9cec8a1b5100ce1e1649ca45d44e27
Dissec <104654480+Dissec@users.noreply.github.com>
2022-05-08 22:45:25 -0300

    Update binds.lua

--- binds.lua
+++ binds.lua
@@ -1,71 +1,15 @@
-"""local space = 0x20
-local shift = 0xA0
-local q = 0x51
-function wallkick()
-    keypress(q)
-    keypress(space)
-    wait(0.15)
-    keyrelease(q)
-    keyrelease(space)
-    v = workspace.joao706.HumanoidRootPart
-    v.Velocity = v.Velocity * Vector3.new(1.2, 0, 1.2)
-end
-function wallboost()
-    keypress(0x20)
-    keyrelease(0x20)
-    mousemoverel(1000, 0)
-    wait()
-    keypress(0x20)
-    keyrelease(0x20)
-    mousemoverel(-1000, 0)
-end
-function cancel()
-    keypress(q)
-    wait(0.12)
-    keyrelease(q)
-    keypress(0x54)
-    wait(0.08)
-    keyrelease(0x54)
-    wait()
-    keypress(0x54)
-    wait()
-    keyrelease(0x54)
-end
-local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
-
-Mouse.KeyDown:connect(function(func)
-
-local key = func:lower()    
-
-if key == "e" then
-    repeat
-    task.wait()
-    until isrbxactive()
-    wallboost()
-end
-if key == "f" then
-    repeat
-    task.wait()
-    until isrbxactive()
-    wallkick()
-end
-if key == "r" then
-    repeat task.wait() until isrbxactive()
-    cancel()
-end
-end)
-"""
 local space = 0x20
 local shift = 0xA0
 local q = 0x51
+local player = game.Players.LocalPlayer.Name
+local rootpart = workspace[player].HumanoidRootPart
 function wallkick()
     keypress(q)
     keypress(space)
     wait(0.15)
     keyrelease(q)
     keyrelease(space)
-    v = workspace.holy1one.HumanoidRootPart
-    v.Velocity = v.Velocity * Vector3.new(1.35, 0, 1.35)
+    rootpart.Velocity = rootpart.Velocity * Vector3.new(1.35, 0, 1.35)
 end
 function wallboost()
     keypress(0x20)
@@ -81,18 +25,20 @@ function reset()
     part.Size = Vector3.new(6, 1, 6)
     part.Transparency = 1
     part.Anchored = true
-    part.Position = workspace.holy1one.HumanoidRootPart.Position - Vector3.new(0, 1.6, 0)
+    part.Position = rootpart.Position - Vector3.new(0, 1.6, 0)
     part.Parent = workspace
     wait(10)
     part:Destroy()
 end
-
-local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
-
-Mouse.KeyDown:connect(function(func)
-
-local key = func:lower()    
-
+function dash()
+    local name = game.Players.LocalPlayer.Name
+    local rootpart = workspace[name].HumanoidRootPart
+    local rotation = game.Workspace.CurrentCamera.CFrame.LookVector
+    rootpart.Velocity = Vector3.new(rotation.x, 0, rotation.z) * 120
+    game.Workspace.Gravity = 5
+    wait(.3)
+    game.Workspace.Gravity = 75
+end
 if key == "e" then
     repeat
     task.wait()
@@ -109,12 +55,14 @@ if key == "r" then
     reset()
 end
 if key == "3" then
-    workspace.holy1one.HumanoidRootPart.Velocity = workspace.holy1one.HumanoidRootPart.Velocity * Vector3.new(1.3, 1.3, 1.3)
+    rootpart.Velocity = rootpart.Velocity * Vector3.new(1.3, 1.3, 1.3)
 end
-end)
-game:GetService("RunService").Heartbeat:Connect(function()
-    local pool = workspace.Pad.Pillow
-    pool.Position = workspace.holy1ne.HumanoidRootPart.Position - Vector3.new(0, 17, 0)
-    pool.Size = Vector3.new(15, 15, 15)
-    pool.Transparency = 1
+local mouse = game.Players.LocalPlayer:GetMouse()
+
+mouse.KeyDown:connect(function(func)
+
+local key = func:lower()
+if key == "q" then
+    dash()
+end
 end)
