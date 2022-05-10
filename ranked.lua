4933d0b4b85537b292ddc8412d90b4a921c59240
Dissec <104654480+Dissec@users.noreply.github.com>
2022-05-10 16:17:53 -0300

    Update ranked.lua

--- ranked.lua
+++ ranked.lua
@@ -5,45 +5,51 @@ local player = game.Players.LocalPlayer.Name
 local rootpart = workspace[player]:WaitForChild("HumanoidRootPart")
 
 function wallkick()
-keypress(q)
-keypress(space)
-wait(0.15)
-keyrelease(q)
-keyrelease(space)
-rootpart.Velocity = rootpart.Velocity * Vector3.new(1.35, 0, 1.35)
+  keypress(q)
+  keypress(space)
+  wait(0.15)
+  keyrelease(q)
+  keyrelease(space)
+  rootpart.Velocity = rootpart.Velocity * Vector3.new(1.35, 0, 1.35)
 end
 function wallboost()
-keypress(0x20)
-keyrelease(0x20)
-mousemoverel(1000, 0)
-wait()
-keypress(0x20)
-keyrelease(0x20)
-mousemoverel(-1000, 0)
+  keypress(0x20)
+  keyrelease(0x20)
+  mousemoverel(1000, 0)
+  wait()
+  keypress(0x20)
+  keyrelease(0x20)
+  mousemoverel(-1000, 0)
 end
 function reset()
-local space = 0x20
-local shift = 0xA0
-local q = 0x51
-local player = game.Players.LocalPlayer.Name
-local rootpart = workspace[player]:WaitForChild("HumanoidRootPart")
-local part = Instance.new("Part")
-part.Size = Vector3.new(6, 1, 6)
-part.Transparency = 1
-part.Anchored = true
-part.Position = rootpart.Position - Vector3.new(0, 1.6, 0)
-part.Parent = workspace
-wait(10)
-part:Destroy()
+  local space = 0x20
+  local shift = 0xA0
+  local q = 0x51
+  local player = game.Players.LocalPlayer.Name
+  local rootpart = workspace[player]:WaitForChild("HumanoidRootPart")
+  local part = Instance.new("Part")
+  part.Size = Vector3.new(6, 1, 6)
+  part.Transparency = 1
+  part.Anchored = true
+  part.Parent = workspace
+  for i = 1,66 do
+    game:GetService("RunService").Heartbeat:Wait()
+    part.Position = rootpart.Position - Vector3.new(0, 1.6, 0)
+  end
+  wait(3)
+  part:Destroy()
 end
 function dash()
-local name = game.Players.LocalPlayer.Name
-local rootpart = workspace[name].HumanoidRootPart
-local rotation = game.Workspace.CurrentCamera.CFrame.LookVector
-rootpart.Velocity = Vector3.new(rotation.x, 0, rotation.z) * 200
-game.Workspace.Gravity = 5
-wait(.3)
-game.Workspace.Gravity = 75
+  local name = game.Players.LocalPlayer.Name
+  local rootpart = workspace[name].HumanoidRootPart
+  local rotation = game.Workspace.CurrentCamera.CFrame.LookVector
+  rootpart.Velocity = Vector3.new(rotation.x, 0, rotation.z) * 130
+  keypress(0x20)
+  wait()
+  keyrelease(0x20)
+  game.Workspace.Gravity = 5
+  wait(.3)
+  game.Workspace.Gravity = 75
 end
 local mouse = game.Players.LocalPlayer:GetMouse()
 
@@ -51,40 +57,40 @@ mouse.KeyDown:connect(function(func)
 
 local key = func:lower()
 if key == "e" then
-repeat
-task.wait()
-until isrbxactive()
-wallboost()
+  repeat
+  task.wait()
+  until isrbxactive()
+  wallboost()
 end
 if key == "f" then
-repeat
-task.wait()
-until isrbxactive()
-wallkick()
+  repeat
+  task.wait()
+  until isrbxactive()
+  wallkick()
 end
 if key == "r" then
-reset()
+  reset()
 end
 if key == "3" then
-local space = 0x20
-local shift = 0xA0
-local q = 0x51
-local player = game.Players.LocalPlayer.Name
-local rootpart = workspace[player]:WaitForChild("HumanoidRootPart")
-rootpart.Velocity = rootpart.Velocity * Vector3.new(1.3, 1.3, 1.3)
+  local space = 0x20
+  local shift = 0xA0
+  local q = 0x51
+  local player = game.Players.LocalPlayer.Name
+  local rootpart = workspace[player]:WaitForChild("HumanoidRootPart")
+  rootpart.Velocity = rootpart.Velocity * Vector3.new(1.3, 1.3, 1.3)
 end
 if key == "q" then
-dash()
+  dash()
 end
 end)
 game:GetService("RunService").Heartbeat:Connect(function()
-local space = 0x20
-local shift = 0xA0
-local q = 0x51
-local player = game.Players.LocalPlayer.Name
-local rootpart = workspace[player]:WaitForChild("HumanoidRootPart")
-local pool = workspace.Pad.Pillow
-pool.Position = rootpart.Position - Vector3.new(0, 17, 0)
-pool.Size = Vector3.new(15, 15, 15)
-pool.Transparency = 1
+  local space = 0x20
+  local shift = 0xA0
+  local q = 0x51
+  local player = game.Players.LocalPlayer.Name
+  local rootpart = workspace[player]:WaitForChild("HumanoidRootPart")
+  local pool = workspace.Pad.Pillow
+  pool.Position = rootpart.Position - Vector3.new(0, 17, 0)
+  pool.Size = Vector3.new(15, 15, 15)
+  pool.Transparency = 1
 end)
