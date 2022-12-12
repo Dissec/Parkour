d959bcf4b0b197aa43ed106fcb2022b89c73dd0b
Dissec <104654480+Dissec@users.noreply.github.com>
2022-12-12 15:34:40 -0300

    .

--- start.lua
+++ start.lua
@@ -1,6 +1,6 @@
 _G.delay = .04
 if not _G.enabled then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Dissec/pk/main/all/binds.lua'. true))
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dissec/pk/main/all/binds.lua", true))
 end
 
 _G.enabled = true
